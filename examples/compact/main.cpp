ed9645876ef1d0c793c9749a66e50e53a620294c
Marcel Smit <marcel303@gmail.com>
2020-03-02 11:16:31 +0100

    Update compact mesh test to use framework.

--- examples/compact/main.cpp
+++ examples/compact/main.cpp
@@ -1,32 +1,26 @@
-#include <allegro.h>
+#include "framework.h"
 #include "marstd.h"
 
-static BITMAP* colourmap;
 static CMatrix matrix;
 static CCompactVertex* vertex_array;
 
 static void create_mesh(CCompactMesh& mesh);
 static void draw_triangle(int v1, int v2, int v3);
-static void draw_line(int v1, int v2, int c);
+static void draw_line(int v1, int v2, Color c);
 
 static int alloc_count = 0;
 void* operator new(size_t size);
 void* operator new[](size_t size);
-void operator delete(void* p);
-void operator delete[](void* p);
+void operator delete(void* p) noexcept;
+void operator delete[](void* p) noexcept;
 
 int main() {
 
-	allegro_init();
+	setupPaths(CHIBI_RESOURCE_PATHS);
 	
 	{
 	
-	install_keyboard();
-	install_mouse();
-	set_color_depth(16);
-	set_gfx_mode(GFX_AUTODETECT, 320, 240, 0, 0);
-	
-	colourmap = create_bitmap(SCREEN_W, SCREEN_H);
+	framework.init(800, 600);
 	
 	CCompactMesh mesh;
 	
@@ -36,8 +30,10 @@ int main() {
 	
 	vertex_array = mesh.vertex;
 	
-	while (!key[KEY_ESC]) {
+	while (!keyboard.wentDown(SDLK_ESCAPE)) {
 	
+		framework.process();
+		
 		static float rx=0.0, ry=0.0, rz=0.0;
 		
 //		rx += 0.00111;
@@ -45,7 +41,8 @@ int main() {
 //		rz += 0.00311;
 		
 		int mx, my;
-		get_mouse_mickeys(&mx, &my);
+		mx = mouse.dx;
+		my = mouse.dy;
 		rx -= my * 0.01;
 		ry += mx * 0.01;
 		
@@ -53,9 +50,9 @@ int main() {
 		matrix.translate(0.0, 0.0, 2.0);
 		matrix.rotate(rx, ry, rz);
 		
-		set_projection_viewport(0, 0, colourmap->w, colourmap->h);
+		framework.beginDraw(0, 0, 0, 0);
 		
-		clear(colourmap);
+		projectPerspective3d(90.0, 0.01, 100.0);
 		
 		for (int i=0; i<mesh.poly_count; ++i) {
 			CCompactPoly* poly = mesh.poly[i];
@@ -63,9 +60,9 @@ int main() {
 			CVector normal;
 			matrix.apply(poly->plane.normal, normal);
 			// FIXME: Must have a mul3 (ala dp3) function in custom implementation!
-			normal[0] -= matrix.m[matrix.depth].t[0];
-			normal[1] -= matrix.m[matrix.depth].t[1];
-			normal[2] -= matrix.m[matrix.depth].t[2];   			
+			normal[0] -= matrix.m[matrix.depth].GetTranslation()[0];
+			normal[1] -= matrix.m[matrix.depth].GetTranslation()[1];
+			normal[2] -= matrix.m[matrix.depth].GetTranslation()[2];
 			CVector vertex;
 			matrix.apply(vertex_array[poly->vertex[0]].p, vertex);
 			float distance = - normal * vertex;
@@ -75,53 +72,50 @@ int main() {
    				poly->flags = 1;				
 			if (poly->flags)   				
 				draw_triangle(poly->vertex[0],
-					      poly->vertex[1],
-					      poly->vertex[2]);
+					poly->vertex[1],
+					poly->vertex[2]);
 		}
 		
 		for (int i=0; i<mesh.edge_count; ++i) {
 			CCompactEdge& edge = mesh.edge[i];
-			int c;							
+			Color c;
 			if (!edge.poly1 || !edge.poly2) {
-				c = makecol(127, 127, 127);
+				c = Color(127, 127, 127);
 			} else if (edge.poly1->flags && edge.poly2->flags) {
 				// Both polygons are visible.
-				continue;				
+				//continue;
 				if (edge.poly1->plane.normal * edge.poly2->plane.normal < 0.0)
-					c = makecol(191, 191, 191);
+					c = Color(191, 191, 191);
 				else
-    					c = makecol(63, 63, 63);					
+					c = Color(63, 63, 63);
 			} else if (edge.poly1->flags || edge.poly2->flags) {
 				// One polygon is visible. Silhouette edge.
-				c = makecol(255, 255, 0);
+				c = Color(255, 255, 0);
 			} else {
 				// No polygons visible.
 				continue;				
-				c = makecol(227, 227, 227);
+				c = Color(227, 227, 227);
 			}
 			draw_line(edge.vertex1, edge.vertex2, c);
 		}
 		
-		blit(colourmap, screen, 0, 0, 0, 0, colourmap->w, colourmap->h);
+		framework.endDraw();
 	
 	}
 	
-	destroy_bitmap(colourmap);
-	
 	}
+	
+	framework.shutdown();
 	
 //	if (alloc_count != 0)
 //		allegro_message("%d allocations left.", alloc_count);
-		
-	allegro_exit();		
 	
 	return alloc_count;
 
-} END_OF_MAIN();
+}
 
 static void create_mesh(CCompactMesh& mesh) {
 
-	const int start = mesh.vertex_count;
 	CCompactVertex vertex;
  	
 	// Add vertices.
@@ -181,75 +175,64 @@ static void create_mesh(CCompactMesh& mesh) {
 
 static void draw_triangle(int v1, int v2, int v3) {
 
-	static int initialized = 0;
-	static V3D_f s_vold[3];    	
-        static V3D_f* vold[3] = { &s_vold[0], &s_vold[1], &s_vold[2] };
-        static V3D_f* vnew[3*8];
-        static V3D_f* vtmp[3*8];
-        static int itmp[3*8];
-        static V3D_f s_vnew[3*8];
-        static V3D_f s_vtmp[3*8];
-
-	// Initialize vertex structures.
-	
-	if (!initialized) {
-	        for (int i=0; i<3*8; i++) {
-        		vnew[i] = &s_vnew[i];
-        		vtmp[i] = &s_vtmp[i];
-	        }
-	        initialized = 1;
-	}	        
-
 	CCompactVertex& vertex1 = vertex_array[v1];
 	CCompactVertex& vertex2 = vertex_array[v2];
 	CCompactVertex& vertex3 = vertex_array[v3];
-  	
+	
 	// Transform.
 	
-        matrix.apply(vertex1.p[0], vertex1.p[1], vertex1.p[2], vold[0]->x, vold[0]->y, vold[0]->z);
-        matrix.apply(vertex2.p[0], vertex2.p[1], vertex2.p[2], vold[1]->x, vold[1]->y, vold[1]->z);
-        matrix.apply(vertex3.p[0], vertex3.p[1], vertex3.p[2], vold[2]->x, vold[2]->y, vold[2]->z);
+	Vec3 transformed[3];
 
-        vold[0]->c = makecol24(255, 255, 255);        
+	matrix.apply(vertex1.p[0], vertex1.p[1], vertex1.p[2], transformed[0][0], transformed[0][1], transformed[0][2]);
+	matrix.apply(vertex2.p[0], vertex2.p[1], vertex2.p[2], transformed[1][0], transformed[1][1], transformed[1][2]);
+	matrix.apply(vertex3.p[0], vertex3.p[1], vertex3.p[2], transformed[2][0], transformed[2][1], transformed[2][2]);
 
-//        vold[0]->c = makecol24(int((sin(vertex1.p[0])+1.0)*0.5*255.0), 0, 0);
-//        vold[1]->c = makecol24(0, int((sin(vertex2.p[1])+1.0)*0.5*255.0), 0);
-//        vold[2]->c = makecol24(0, 0, int((sin(vertex3.p[2])+1.0)*0.5*255.0));        
-
-	// Clip.
+	// Colorize.
 	
-        const int vc = clip3d_f(POLYTYPE_FLAT, 0.01, 10.0, 3, (const V3D_f** )vold, vnew, vtmp, itmp);
-
-        if (vc < 3)
-        	return;
-
-	// Project first three vertices.
+	Color color[3];
 	
-       	persp_project_f(vnew[0]->x, vnew[0]->y, vnew[0]->z, &vnew[0]->x, &vnew[0]->y);
-       	persp_project_f(vnew[1]->x, vnew[1]->y, vnew[1]->z, &vnew[1]->x, &vnew[1]->y);
-       	persp_project_f(vnew[2]->x, vnew[2]->y, vnew[2]->z, &vnew[2]->x, &vnew[2]->y);                 	
-       	
-       	// Project the rest.
-       	
-	for (int i=3; i<vc; i++)
-        	persp_project_f(vnew[i]->x, vnew[i]->y, vnew[i]->z, &vnew[i]->x, &vnew[i]->y);
+#if 0
 
-	polygon3d_f(colourmap, POLYTYPE_FLAT, 0, vc, vnew);
+	color[0] = Color(255, 255, 255);
+	color[1] = color[0];
+	color[2] = color[0];
+
+#else
+
+	color[0] = Color(int((sin(vertex1.p[0])+1.0)*0.5*255.0), 0, 0);
+	color[1] = Color(0, int((sin(vertex2.p[1])+1.0)*0.5*255.0), 0);
+	color[2] = Color(0, 0, int((sin(vertex3.p[2])+1.0)*0.5*255.0));
+
+#endif
+
+	gxBegin(GX_TRIANGLES);
+	{
+	
+		for (int i = 0; i < 3; ++i)
+		{
+		
+			gxColor4fv(&color[i].r);
+			gxVertex3fv(&transformed[i][0]);
+			
+		}
+		
+	}
+	gxEnd();
 
 }
 
-static void draw_line(int v1, int v2, int c) {
+static void draw_line(int v1, int v2, Color c) {
 
 	float x1, y1, z1;
 	float x2, y2, z2;
 	
-        matrix.apply(vertex_array[v1].p[0], vertex_array[v1].p[1], vertex_array[v1].p[2], x1, y1, z1);
-        matrix.apply(vertex_array[v2].p[0], vertex_array[v2].p[1], vertex_array[v2].p[2], x2, y2, z2);
+	matrix.apply(vertex_array[v1].p[0], vertex_array[v1].p[1], vertex_array[v1].p[2], x1, y1, z1);
+	matrix.apply(vertex_array[v2].p[0], vertex_array[v2].p[1], vertex_array[v2].p[2], x2, y2, z2);
 
 	// Do clipping manually.
          
-        if (z1 <= 0.01 && z2 <= 0.01)
-        	return;
+	if (z1 <= 0.01 && z2 <= 0.01)
+		return;
         	
 	if (z1 < 0.01 && z2 > 0.01) {
 		float t = (0.01 - z1) / (z2 - z1);
@@ -263,18 +246,17 @@ static void draw_line(int v1, int v2, int c) {
 		z2 = z2 + (z2 - z1) * t;  		
 	}
 	
-	// Clipping against other planes is not neccessary.
-	
-	// Project.
-	
-	persp_project_f(x1, y1, z1, &x1, &y1);
-	persp_project_f(x2, y2, z2, &x2, &y2);
-	
 	// Render!
 
-	line(colourmap, (int)x1, (int)y1, (int)x2, (int)y2, c);	
-	line(colourmap, (int)x1, (int)y1-1, (int)x2, (int)y2-1, c);
-	line(colourmap, (int)x1, (int)y1+1, (int)x2, (int)y2+1, c);	
+	gxBegin(GX_LINES);
+	{
+	
+		setColor(c);
+		gxVertex3f(x1, y1, z1);
+		gxVertex3f(x2, y2, z2);
+		
+	}
+	gxEnd();
 
 }
 
@@ -285,10 +267,10 @@ void* operator new(size_t size) {
 void* operator new[](size_t size) {
 	return operator new(size);
 }
-void operator delete(void* p) {
+void operator delete(void* p) noexcept {
 	free(p);
 	alloc_count--;
 }
-void operator delete[](void* p) {
+void operator delete[](void* p) noexcept {
 	operator delete(p);
 }
